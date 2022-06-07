db9e5304332898967dee8eb4957721d6b8b38692
Qiushi Peng <pengqiushi1998@gmail.com>
2022-06-07 15:44:39 -0700

    Update extract_country.py
    Use package "geograpy" instead of "geotext".

--- scripts/extract_country.py
+++ scripts/extract_country.py
@@ -1,11 +1,22 @@
 import os
 import xml.etree.ElementTree as ET
-from geotext import GeoText
+from geograpy import extraction, places
 
 path = '/scratch1/qiushipe/data_reusability/publications_sorted'
 file_names = os.listdir(path)
 
-def extract_country(file):
+
+### extract countries from a given string
+def extract_country(text):
+    e = extraction.Extractor(text = text)
+    e.find_geoEntities()
+    plc = places.PlaceContext(e.places)
+    plc.set_countries()
+    return plc.countryies[-1]
+
+
+### find all countries mentioned in a xml file
+def parse_file(file):
     try:
         root = ET.parse(file).getroot()
     except:
@@ -17,25 +28,26 @@ def extract_country(file):
     for i in range(len(labels)):
         aff = labels[i].tail
             #  e.g. Unit of Health Sciences and Education, University of Hamburg, Martin-Luther-King Platz 6, 20146 Hamburg, Germany
-        country = GeoText(aff).countries[0]
+        country = extract_country(aff)
         countries.append(country)
     
-    ### different arrangement in xml files
+    ## different arrangement in xml files
     if countries == []:
         institution_wrap = root.findall('./front/article-meta/contrib-group/aff/institution-wrap')
         for i in range(len(institution_wrap)):
             aff = institution_wrap[i].tail
-            country = GeoText(aff).countries[0]
+            country = extract_country(aff)
             countries.append(country)
     else:
         pass
 
     return countries
 
+
 for file_name in file_names:
     file = open(path + '/' + file_name)
     try:
-        country = extract_country(file)[-1]
+        country = parse_file(file)[-1]
     except:
         country = 'N/A'
     pmc_ID = file_name.split('.')[0]
